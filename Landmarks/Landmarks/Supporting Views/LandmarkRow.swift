105c1a3ba4a5886d6fd5eaa86f75658eae93cced
spencer1573 <spencer1573@users.noreply.github.com>
2019-10-26 20:22:57 -0600

    fixed error with not showing upload

---
+++ Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by spencer on 10/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
